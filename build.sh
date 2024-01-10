40a46316c99850e44ba61e9a53d633b837b95b70
H3kapoo <andrei.boje00@e-uvt.ro>
2024-01-10 13:51:14 +0200

    readme

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cmake -B build -GNinja . # generate ninja data
-cmake --build build      # link and compile
-mv debug/hkui .      # move out of debug folder
-# ./simpleUI               # run at root level
+cmake -B build -GNinja .            # generate ninja data
+cmake --build build                 # link and compile
+rm -rf ./lib
+mkdir ./lib 
+mv build/libhkui.a ./lib/libhkui.a  # move out of debug folder
